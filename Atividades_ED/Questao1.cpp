13242c8c6c9b3deed1487b524644ac82d1454384
Natan dos Santos Nogueira <165215241+Natan-S-Nogueira@users.noreply.github.com>
2024-09-02 21:10:33 -0300

    Add files via upload

---
+++ Atividades_ED/Questao1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int converte_em_min(int hora, int minuto) {
+	int h = hora * 60;
+	int m = minuto;
+	int converte = h + m;
+	return converte;
+}
+
+int main () {
+	int hora, minuto;
+	for (int i = 0; i <= 2; i++) {
+		printf("Digite a hora: \n");
+		scanf("%d", &hora);
+		printf("Digite o minuto: \n");
+		scanf("%d", &minuto);
+	
+		int hora_convertida = converte_em_min(hora, minuto);
+	
+		printf("Hora original: %dh e %dmin\n", hora, minuto);
+		printf("Hora convertida: %dmin\n", hora_convertida);
+	
+		i++;
+	}
+	
+}
